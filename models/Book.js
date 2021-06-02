0e0c615a988ee3641a292a02c0010b466ff87f84
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-06-02 14:21:43 -0500

    Additional setup in the Server and in routing in Client.

---
+++ models/Book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  authors: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -1,11 +1,44 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require("path");
+const mongoose = require("mongoose");
+
+
 const port = process.env.PORT || 8000;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.use(cors());
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactrecipes",
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
+
+app.get('/api/books', (req, res) => {
+  const url = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyDX0AkFlYKSwPlr-BOhGdpH9O6lxiWBDNE";
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Use apiRoutes
+app.use("/api", apiRoutes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
