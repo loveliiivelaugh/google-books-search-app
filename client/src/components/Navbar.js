0bdf6a5aa2efdf26465dbd043f157f8ef4e0a880
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-06-07 07:44:51 -0500

    Successfully fetching data. Completed Results component.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Button, Card, CardBody, Container, Form, FormGroup, Label, Input, FormText, Jumbotron, 
+  Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText 
+} from 'reactstrap';
 
-const Navbar = () => {
+const Navigator = () => {
+    
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Google Books</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Search</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Saved</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
   )
 }
 
-export default Navbar
+export default Navigator
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {  
+  Button, Card, CardBody, CardTitle, Container, 
+  Form, FormGroup, Label, Input, FormText, Jumbotron, 
+  Col, Row, CardText, CardSubtitle
+} from 'reactstrap';
+
 
 const SearchPage = () => {
   const [booksData, setBooks] = useState([]);
@@ -7,12 +13,13 @@ const SearchPage = () => {
   const fetchData = async (searchTerm) => {
     const booksData = await axios.get('/api/books/' + searchTerm);
 
+    console.log(booksData)
     setBooks(booksData.data);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -22,12 +29,69 @@ const SearchPage = () => {
   };
 
   console.log(booksData);
-  
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="search" />
-      <button type="submit">Search</button>
-    </form>
+    <Container>
+
+    {/* HeroSection */}
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">React Google Books Search</h1>
+          <p className="lead">Search for and Save Books of Interest</p>
+        </Container>
+      </Jumbotron>
+
+    {/* SearchSection */}
+      <Card className="p-3">
+        <CardTitle tag="h5">Book Search</CardTitle>
+        <CardBody>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="search">Book</Label>
+              <Input type="text" name="search" id="search" placeholder="Search for a book..." />
+            </FormGroup>
+            <FormGroup>
+              <Button type="submit">Search</Button>
+            </FormGroup>
+          </Form>
+        </CardBody>
+      </Card>
+
+    {/* ResultsSection */}
+    {booksData.length > 0 && 
+      <Card className="p-3">
+        <CardTitle tag="h5">Results</CardTitle>
+        <CardBody>
+          {booksData && booksData.map(({ volumeInfo }) => (
+            <Card className="p-2">
+              <Row className="m-3">
+                <Col md={6}>
+                  <CardTitle tag="h5">{volumeInfo.title}</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">{volumeInfo.subtitle}</CardSubtitle>
+                  <CardText><small>Written By: {volumeInfo.authors[0]}</small></CardText>
+                </Col>
+                <Col md={6}>
+                  <Button>View</Button>
+                  <Button>Save</Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Container>
+                    <img src={volumeInfo.imageLinks.smallThumbnail} alt={volumeInfo.title} />
+                  </Container>
+                </Col>
+                <Col>
+                <CardText>{volumeInfo.description}</CardText>
+                </Col>
+              </Row>
+            </Card>
+          ))}
+        </CardBody>
+      </Card>
+    }
+
+    </Container>
   )
 }
 
